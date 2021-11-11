e851c727a5930145a41d13f8420329c5e5776500
behnam <behnam74gh@yahoo.com>
2021-11-11 05:00:04 +0330

    header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdNotifications, MdApps } from "react-icons/md";
+
+import ProfileFakeImg from "../../assets/images/pro-8.png";
 import "./_header.scss";
 
 const Header = () => {
   return (
-    <div className="border border-dark">
-      <h1>Header</h1>
+    <div className="border border-dark header">
+      <FaBars className="header__menu" size={26} />
+      <img
+        src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+        alt="UT.Logo"
+        className="header__logo"
+      />
+      <form>
+        <input type="search" placeholder="Search.." />
+        <button type="submit">
+          <AiOutlineSearch size={22} />
+        </button>
+      </form>
+      <div className="header__icons">
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img src={ProfileFakeImg} alt="avatar" />
+      </div>
     </div>
   );
 };
