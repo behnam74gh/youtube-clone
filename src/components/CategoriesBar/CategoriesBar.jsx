682b819de31dbaa62d57bab78cccb661392be9a3
behnam <behnam74gh@yahoo.com>
2021-11-11 06:46:25 +0330

    categoriesbar completed

--- src/components/CategoriesBar/CategoriesBar.jsx
+++ src/components/CategoriesBar/CategoriesBar.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_CategoriesBar.scss";
 
+const keywords = [
+  "All",
+  "React js",
+  "Angular js",
+  "React Native",
+  "use of api",
+  "Redux",
+  "Music",
+  "Algorithm Art",
+  "Guitar",
+  "Coding",
+  "Footbal",
+  "Node.js",
+  "Express.js",
+  "MongoDB",
+  "Mongoose",
+  "Sass",
+  "typescript",
+];
+
 const CategoriesBar = () => {
+  const [activeElement, setActiveElement] = useState("All");
+
+  const changeActiveElementHandler = (item) => {
+    setActiveElement(item);
+  };
+
   return (
-    <div>
-      <h4>CategoriesBar</h4>
+    <div className="categories_bar">
+      {keywords.map((item, i) => (
+        <span
+          key={i}
+          onClick={() => changeActiveElementHandler(item)}
+          className={activeElement === item ? "active" : ""}
+        >
+          {item}
+        </span>
+      ))}
     </div>
   );
 };
